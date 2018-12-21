03464309cb178849a216ec32ca2a1fafa0ef2ad6
Federico Miras <fefo@decentraland.org>
2018-12-21 09:21:30 -0300

    chore: setup truffle mainnet network and update deps

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
-'use strict';
+'use strict'
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const INFURA_TOKEN = '<TOKEN>'
+const mnemonic = 'orange apple banana...'
 
 module.exports = {
   networks: {
@@ -8,6 +12,12 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       network_id: '*'
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${INFURA_TOKEN}`)
+      },
+      network_id: 1
     }
   }
-};
+}
